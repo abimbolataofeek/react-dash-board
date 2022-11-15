e1e4bea1c529734f90e93a57223ac0625dc11280
abimbolataofeek <abimbolaadwale9999@gmail.com>
2022-11-15 18:15:53 +0100

    completed the chart Route

--- src/App.js
+++ src/App.js
@@ -45,9 +45,26 @@ const App = () => {
               <Route path='/ecommerce' element="ECommerce" />
 
               {/* Pages */}
-              <Route path="/orders" element="Orders"/>
+              <Route path="/orders" element="Orders" />
               <Route path='/employees' element="Employess" />
               <Route path='/customers' element="Customers" />
+
+            {/* Apps */}
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calender" element="Calender" />
+              <Route path="/color-picker" element="ColorPicker" />
+
+
+              {/* Charts*/}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
 
