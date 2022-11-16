6c116dc8269310989fcc270faf4046d19d1d82b5
abimbolataofeek <abimbolaadwale9999@gmail.com>
2022-11-16 11:23:15 +0100

    folder structure complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import{Navbar, Footer, Sidebar, ThemeSettings } from './components';
+
+import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban,
+Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+
 import './App.css'
 
 const App = () => {
@@ -49,7 +55,7 @@ const App = () => {
               <Route path='/employees' element="Employess" />
               <Route path='/customers' element="Customers" />
 
-            {/* Apps */}
+              {/* Apps */}
               <Route path="/kanban" element="Kanban" />
               <Route path="/editor" element="Editor" />
               <Route path="/calender" element="Calender" />
